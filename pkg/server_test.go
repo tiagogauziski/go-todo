683f9adb2352e8a70c82d489922271de3a1e3d95
Tiago Gauziski <tiago.gauziski@gmail.com>
2023-05-16 22:00:24 +1200

    wip test code.

--- pkg/server_test.go
+++ pkg/server_test.go
@@ -12,9 +12,8 @@ func TestPingRoute(t *testing.T) {
 	router := SetupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
+	req, _ := http.NewRequest("GET", "/todos", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
 }
